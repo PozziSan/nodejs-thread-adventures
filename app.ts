6a7182137b91443e7644699d205d53c84b470eef
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2025-05-24 11:38:45 -0300

    Start some threads

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-void (() => console.log("I'm running typescript natively!"))();
+import { Worker } from "worker_threads";
+
+const THREAD_FILE = "./thread.ts";
+
+void (() => {
+    const thread1 = new Worker(THREAD_FILE);
+    const thread2 = new Worker(THREAD_FILE);
+
+    const finishMessage = (id: string) => console.log(`thread ${id} finished`);
+
+    thread1.on("exit", () => finishMessage("thread1"));
+    thread2.on("exit", () => finishMessage("thread2"));
+})();

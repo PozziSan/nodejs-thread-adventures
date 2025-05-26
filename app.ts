94de8b4e98cbb772312884ade524a4d379433201
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2025-05-26 12:24:57 -0300

    Share Messages between threads

--- app.ts
+++ app.ts
@@ -1,13 +1,23 @@
-import { Worker } from "worker_threads";
+import { Worker } from 'worker_threads';
+import { MessageChannel } from 'worker_threads';
 
-const THREAD_FILE = "./thread.ts";
+const THREAD_FILE = './thread.ts';
+const { port1, port2 } = new MessageChannel();
 
 void (() => {
-    const thread1 = new Worker(THREAD_FILE);
-    const thread2 = new Worker(THREAD_FILE);
+    const thread1 = new Worker(THREAD_FILE, { workerData: { port: port1, name: 'thread1' }, transferList: [port1] });
+    const thread2 = new Worker(THREAD_FILE, { workerData: { port: port2, name: 'thread2' }, transferList: [port2] });
+
+    setTimeout(() => {
+        console.log('Hasta la vista');
+        const message = 'exit';
+
+        thread1.postMessage(message);
+        thread2.postMessage(message);
+    }, 7000);
 
     const finishMessage = (id: string) => console.log(`thread ${id} finished`);
 
-    thread1.on("exit", () => finishMessage("thread1"));
-    thread2.on("exit", () => finishMessage("thread2"));
+    thread1.on('exit', () => finishMessage('thread1'));
+    thread2.on('exit', () => finishMessage('thread2'));
 })();
--- thread.ts
+++ thread.ts
@@ -1,1 +1,25 @@
-console.log('This is Thread');
+import { workerData, parentPort } from 'worker_threads';
+
+import type { MessagePort } from 'worker_threads';
+
+type workerDataType = {
+    port: MessagePort;
+    name: string;
+};
+
+const { port, name } = workerData as workerDataType;
+
+port.postMessage(`Hello from ${name}`);
+
+port.on('message', (msg: string) => {
+    console.log(`Received message on ${name}: ${msg}`);
+});
+
+parentPort?.on('message', (msg: string) => {
+    if (msg === 'exit') {
+        console.log('exit message received on', name);
+        process.exit(0);
+    }
+
+    console.log(`Received message from parent: ${msg}`);
+});
